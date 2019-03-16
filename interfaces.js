a80281920549fa25192788392e8167cbe5592723
Deepak <er.d.ahuja@gmail.com>
2019-03-16 18:21:53 +0530

    add tsc

---
+++ interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+;
+var Type;
+(function (Type) {
+    Type[Type["Quiz"] = 0] = "Quiz";
+    Type[Type["Video"] = 1] = "Video";
+    Type[Type["Blog"] = 2] = "Blog";
+})(Type = exports.Type || (exports.Type = {}));
+;
