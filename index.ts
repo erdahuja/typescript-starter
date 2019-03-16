ab42493b233a8f1a26fb5792d19359e5cda841b7
Deepak <er.d.ahuja@gmail.com>
2019-03-16 11:35:56 +0530

    basic types

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ enum Job {
 const job: Job = Job.WebDev;
 
 let phone: any = 'Pixel';
-// const tablet: never = 3;
+const tablet: never = 3;
 console.log(typeof phone);
 phone = 123;
 console.log(typeof phone);
