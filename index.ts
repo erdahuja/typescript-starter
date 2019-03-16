597e2245b5c7e77cde0637a26960f5a2161fe19f
Deepak <er.d.ahuja@gmail.com>
2019-03-16 11:29:43 +0530

    basic types

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-const isOpen: boolean = true;
-const myName: string = "Deepak";
-const myAge: number = 24;
-const list: number[] = [0,1,2];
+const isOpen: boolean = false;
 
+const myName: string = 'Scott';
 
-console.log(isOpen);
+const myAge: number = 32.04;
+
+const list: number[] = [0, 1, 2];
+
+const me: [string, number, boolean] = ['Scott', 32, false];
+
+enum Job {
+  WebDev,
+  WebDesigner,
+  PM
+}
+const job: Job = Job.WebDev;
+
+let phone: any = 'Pixel';
+// const tablet: never = 3;
+console.log(typeof phone);
+phone = 123;
+console.log(typeof phone);
